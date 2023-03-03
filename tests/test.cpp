1662d8026f75db56ebaca318c6529192100fc128
dngo2401 <doinngo@gmail.com>
2023-03-03 15:03:45 -0800

    Removed Movie_list and Review_list duplicate stubs

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,8 +3,3 @@
 #include "../header/User.hpp"
 #include "../header/Movie_list.hpp"
 #include "../header/Review_list.hpp"
-
-TEST(MovieTests, ConstructorTest) {
-    User newUser("Bob", "password");
-    EXPECT_EQ(newUser.print_credentials(), "Bob password");
-}
